7ab3ea495230192d67cf25965dc512c386dcf1d8
sLomanojlo <slobodanmanojlovic@gmail.com>
2017-10-04 17:24:10 +0200

    Code inspection - hardcoded strings extracted to strings.xml - unused imports deleted - unused layout_align deleted - other minor modifications

--- app/src/main/java/com/example/korisnik/findme/FindCarActivity.java
+++ app/src/main/java/com/example/korisnik/findme/FindCarActivity.java
@@ -1,11 +1,8 @@
 package com.example.korisnik.findme;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TabHost;
@@ -27,7 +24,7 @@ public class FindCarActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
-        host = (TabHost) findViewById(R.id.tabHost);
+        host = (TabHost)findViewById(R.id.tabHost);
         host.setup();
 
         //Tab 1
@@ -48,47 +45,48 @@ public class FindCarActivity extends AppCompatActivity {
         spec.setIndicator("Vans");
         host.addTab(spec);
 
-        String[] items = new String[]{"One", "Two", "Three"};
+        String[] items = new String[] {"One", "Two", "Three"};
         Spinner spinnerMake = (Spinner) findViewById(R.id.spinnerMake);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, items);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerMake.setAdapter(adapter);
 
-        String[] items2 = new String[]{"One", "Two", "Three"};
+        String[] items2 = new String[] {"One", "Two", "Three"};
         Spinner spinnerModel = (Spinner) findViewById(R.id.spinnerModel);
         ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, items);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerModel.setAdapter(adapter);
 
-        String[] items3 = new String[]{"One", "Two", "Three"};
-        Spinner spinnerMinPrice = (Spinner) findViewById(R.id.spinnerMinPrice);
+        String[] items3 = new String[] {"One", "Two", "Three"};
+        Spinner spinnerMinPrice= (Spinner) findViewById(R.id.spinnerMinPrice);
         ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, items);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerMinPrice.setAdapter(adapter);
 
-        String[] items4 = new String[]{"One", "Two", "Three"};
+        String[] items4 = new String[] {"One", "Two", "Three"};
         Spinner spinnerMaxPrice = (Spinner) findViewById(R.id.spinnerMaxPrice);
         ArrayAdapter<String> adapter4 = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, items);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerMaxPrice.setAdapter(adapter);
 
-        String[] items5 = new String[]{"One", "Two", "Three"};
+        String[] items5 = new String[] {"One", "Two", "Three"};
         Spinner spinnerMilage = (Spinner) findViewById(R.id.spinnerMilage);
         ArrayAdapter<String> adapter5 = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, items);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerMilage.setAdapter(adapter);
 
-        String[] items6 = new String[]{"One", "Two", "Three"};
+        String[] items6 = new String[] {"One", "Two", "Three"};
         Spinner spinnerYearManufacture = (Spinner) findViewById(R.id.spinnerYearManufacture);
         ArrayAdapter<String> adapter6 = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, items);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerYearManufacture.setAdapter(adapter);
+
     }
 
 
--- app/src/main/java/com/example/korisnik/findme/RefineSearch1Activity.java
+++ app/src/main/java/com/example/korisnik/findme/RefineSearch1Activity.java
@@ -9,6 +9,10 @@ import android.widget.Spinner;
 
 public class RefineSearch1Activity extends AppCompatActivity {
 
+    public static final String one = "One";
+    public static final String two = "Two";
+    public static final String three = "Three";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,21 +21,21 @@ public class RefineSearch1Activity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
-        String[] items = new String[] {"One", "Two", "Three"};
+        String[] items = new String[] {one, two, three};
         Spinner spinnerMake = (Spinner) findViewById(R.id.spinnerMake);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, items);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerMake.setAdapter(adapter);
 
-        String[] items2 = new String[] {"One", "Two", "Three"};
+        String[] items2 = new String[] {one, two, three};
         Spinner spinnerModel = (Spinner) findViewById(R.id.spinnerModel);
         ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, items2);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerModel.setAdapter(adapter);
 
-        String[] items3 = new String[] {"One", "Two", "Three"};
+        String[] items3 = new String[] {one, two, three};
         Spinner spinnerNewUsed = (Spinner) findViewById(R.id.spinnerNewUsed);
         ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, items2);
