882facf0366e514d69a052114d500189ea3990e8
sLomanojlo <slobodanmanojlovic@gmail.com>
2017-10-04 10:07:25 +0200

    activity_find_car.xml: - initial version completed

--- app/src/main/java/com/example/korisnik/findme/FindCarActivity.java
+++ app/src/main/java/com/example/korisnik/findme/FindCarActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TabHost;
 
 /**
@@ -25,18 +27,8 @@ public class FindCarActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
-        host = (TabHost)findViewById(R.id.tabHost);
+        host = (TabHost) findViewById(R.id.tabHost);
         host.setup();
-//        host.setOnTabChangedListener(new TabHost.OnTabChangeListener(){
-//            @Override
-//            public void onTabChanged(String tabId) {
-//                if(tab1.equals(tabId)) {
-//                    //destroy earth
-//                }
-//                if(TAB_2_TAG.equals(tabId)) {
-//                    //destroy mars
-//                }
-//            }});
 
         //Tab 1
         TabHost.TabSpec spec = host.newTabSpec("Cars");
@@ -56,18 +48,48 @@ public class FindCarActivity extends AppCompatActivity {
         spec.setIndicator("Vans");
         host.addTab(spec);
 
+        String[] items = new String[]{"One", "Two", "Three"};
+        Spinner spinnerMake = (Spinner) findViewById(R.id.spinnerMake);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, items);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerMake.setAdapter(adapter);
 
+        String[] items2 = new String[]{"One", "Two", "Three"};
+        Spinner spinnerModel = (Spinner) findViewById(R.id.spinnerModel);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, items);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerModel.setAdapter(adapter);
+
+        String[] items3 = new String[]{"One", "Two", "Three"};
+        Spinner spinnerMinPrice = (Spinner) findViewById(R.id.spinnerMinPrice);
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, items);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerMinPrice.setAdapter(adapter);
+
+        String[] items4 = new String[]{"One", "Two", "Three"};
+        Spinner spinnerMaxPrice = (Spinner) findViewById(R.id.spinnerMaxPrice);
+        ArrayAdapter<String> adapter4 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, items);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerMaxPrice.setAdapter(adapter);
+
+        String[] items5 = new String[]{"One", "Two", "Three"};
+        Spinner spinnerMilage = (Spinner) findViewById(R.id.spinnerMilage);
+        ArrayAdapter<String> adapter5 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, items);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerMilage.setAdapter(adapter);
+
+        String[] items6 = new String[]{"One", "Two", "Three"};
+        Spinner spinnerYearManufacture = (Spinner) findViewById(R.id.spinnerYearManufacture);
+        ArrayAdapter<String> adapter6 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, items);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerYearManufacture.setAdapter(adapter);
     }
 
-//    public static void setTabColor(TabHost host) {
-//        for(int i=0;i<host.getTabWidget().getChildCount();i++) {
-//            host.getTabWidget().getChildAt(i).setBackgroundColor(Color.parseColor("#FF0000")); //unselected
-//        }
-//        host.getTabWidget().getChildAt(host.getCurrentTab()).setBackgroundColor(Color.parseColor("#0000FF")); // selected
-//    }
 
-
-//
-//
-//    }
 }
