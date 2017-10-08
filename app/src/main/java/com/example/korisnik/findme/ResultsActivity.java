04a946aff62deb7cd06373eb441fedf77b9f53ce
sLomanojlo <slobodanmanojlovic@gmail.com>
2017-10-08 17:03:31 +0200

    activity_results.xml: - row_results added

--- app/src/main/java/com/example/korisnik/findme/ResultsActivity.java
+++ app/src/main/java/com/example/korisnik/findme/ResultsActivity.java
@@ -1,9 +1,11 @@
 package com.example.korisnik.findme;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TabHost;
+import android.widget.TextView;
 
 import static android.R.attr.host;
 
@@ -44,5 +46,23 @@ public class ResultsActivity extends AppCompatActivity {
         spec.setContent(R.id.tab3);
         spec.setIndicator("Price (Highest)");
         host.addTab(spec);
+
+//        host.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+//
+//            @Override
+//            public void onTabChanged(String tabId) {
+//
+//                for (int i = 0; i < host.getTabWidget().getChildCount(); i++) {
+//                    host.getTabWidget().getChildAt(i).setBackgroundColor(Color.parseColor("#FF0000")); // unselected
+//                    TextView tv = (TextView) host.getTabWidget().getChildAt(i).findViewById(android.R.id.title); //Unselected Tabs
+//                    tv.setTextColor(Color.parseColor("#ffffff"));
+//                }
+//
+//                host.getTabWidget().getChildAt(host.getCurrentTab()).setBackgroundColor(Color.parseColor("#0000FF")); // selected
+//                TextView tv = (TextView) host.getCurrentTabView().findViewById(android.R.id.tabhost); //for Selected Tab
+//                tv.setTextColor(Color.parseColor("#000000"));
+//
+//            }
+//        });
     }
 }
